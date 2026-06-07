ca46d961946dd8c6b6d2598aad8b0f57cd791572
rockyxd3 <shivamlkr629@gmail.com>
2026-06-07 16:11:02 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,24 @@
+import os
+from os import getenv
+
+API_ID = int(getenv("API_ID", "39934421")) 
+API_HASH = getenv("API_HASH", "407aaa4db92ab5d0b32027d6482a5fff")
+BOT_TOKEN = getenv("BOT_TOKEN", "8395982941:AAHceR5rWsxcai9aRf5n2kawEZg0RDUCuLU")
+
+MONGO_URI = getenv("MONGO_URI", "mongodb+srv://odduscode02_db_user:6Ldc6lxc4DWLkfzR@cluster0.8azyhaj.mongodb.net/?appName=Cluster0")
+
+ADMINS = [8238387029, 1399616354]
+STATIC_2FA_PASSWORD = "toxictg"
+ADMIN_GROUP_ID = -1003747918681 
+
+USDT_RATE = 90.0  
+
+PAYMENT_UPI_ID = "BHARATPE.8TOB1B2E2Z16787@fbpe"
+BINANCE_ID = "1200101076"
+TRC20_ADDRESS = "TEXjaYKjW8w9QTZCGkruP9rxT9g4SGzVLa"
+
+LOG_CHANNEL = -1003809723218 
+DEFAULT_FSUB_ID = -1003817817061 
+DEFAULT_FSUB_LINK = "https://t.me/+cIZcyypEQ6g1Mjc1"
+
+DIVIDER = "━━━━━━━━━━━━━━━━━━━━━━"
