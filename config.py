63a3dc029b4e545a537e4668912eaeb616cb0c41
rockyxd3 <shivamlkr629@gmail.com>
2026-06-08 00:05:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from os import getenv
 
 API_ID = int(getenv("API_ID", "24790031")) 
 API_HASH = getenv("API_HASH", "0c7a496a6e33e862be48af651b935cce")
-BOT_TOKEN = getenv("BOT_TOKEN", "8647875756:AAHibWba-_S9bXVZ-Mr4nzoNfW6sbPutWs4")
+BOT_TOKEN = getenv("BOT_TOKEN", "8834012010:AAEoY4ZEDseOIfe8m50SFTgRpyMR-CUtCp4")
 
 MONGO_URI = getenv("MONGO_URI", "mongodb+srv://Mecobot:Mecobots@cluster0.o64pd7z.mongodb.net/?retryWrites=true&w=majority")
 
