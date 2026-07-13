0ca24bee532e390daa61ca685ba2b3c38c7369fa
devilharshit02-glitch <devilharshit02@gmail.com>
2026-07-14 00:36:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,17 +3,17 @@ from os import getenv
 
 API_ID = int(getenv("API_ID", "24790031")) 
 API_HASH = getenv("API_HASH", "0c7a496a6e33e862be48af651b935cce")
-BOT_TOKEN = getenv("BOT_TOKEN", "your_token")
+BOT_TOKEN = getenv("BOT_TOKEN", "8874094390:AAH2cjggJuilxLhLo8zTqdsZufNRgT0_Qck")
 
 MONGO_URI = getenv("MONGO_URI", "mongodb+srv:@cluster0.o64pd7z.mongodb.net/?retryWrites=true&w=majority")
 
-ADMINS = [7632048577, 8867837503, 8935584927]
-STATIC_2FA_PASSWORD = "7979"
-ADMIN_GROUP_ID = -1003661047010
+ADMINS = [8714869727, 8867837503, 8935584927]
+STATIC_2FA_PASSWORD = "4444"
+ADMIN_GROUP_ID = -1004322139633
 
 USDT_RATE = 90.0  
 
-PAYMENT_UPI_ID = "abhishek.kr.tg@fam"
+PAYMENT_UPI_ID = "harsh.shukla.inn@okicici"
 BINANCE_ID = "1193032529"
 TRC20_ADDRESS = "TN3J2Ut6NNkqpWuy7m61UfG1oQG2onXLnm"
 
